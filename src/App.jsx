313b54720a939a992e16b9e2228252faf1a5b387
Federico Spadaro <federicospadaro2@gmail.com>
2025-02-28 17:44:26 +0100

    [TEST] layout on MovieCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
 import SearchBar from "./components/SearchBar";
 import MovieList from "./components/MovieList";
 
 const API_KEY = "3d2f7b4506c7c6af335517dd5f8c25b0";
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
 
 const LANGUAGE_CODES = {
   en: "gb",
@@ -12,42 +13,21 @@ const LANGUAGE_CODES = {
   es: "es",
   de: "de",
   ja: "jp",
-  ko: "kr"
+  ko: "kr",
 };
 
 function App() {
   const [results, setResults] = useState([]);
-  const [imageBaseUrl, setImageBaseUrl] = useState(""); 
-
-  useEffect(() => {
-    const fetchImageBaseUrl = async () => {
-      try {
-        const response = await axios.get(
-          `https://api.themoviedb.org/3/configuration?api_key=${API_KEY}`
-        );
-        const baseUrl = response.data.images.base_url; 
-        setImageBaseUrl(baseUrl); 
-      } catch (error) {
-        console.error("Errore nel recupero dell'URL base delle immagini", error);
-      }
-    };
-
-    fetchImageBaseUrl();
-  }, []);
 
   const searchMedia = async (query) => {
     if (!query) return;
-
     try {
       const movieResponse = await axios.get(
         `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}&language=it-IT`
       );
-
       const tvResponse = await axios.get(
         `https://api.themoviedb.org/3/search/tv?api_key=${API_KEY}&query=${query}&language=it-IT`
       );
-
-      // Unire i risultati di film e serie
       const combinedResults = [...movieResponse.data.results, ...tvResponse.data.results];
       setResults(combinedResults);
     } catch (error) {
@@ -56,12 +36,15 @@ function App() {
   };
 
   return (
-    <div>
-      <h1>BoolFlix</h1>
-      <SearchBar onSearch={searchMedia} />
-      <MovieList movies={results} languageCodes={LANGUAGE_CODES} imageBaseUrl={imageBaseUrl} />
+    <div className="app">
+      <header className="app-header">
+        <h1>BoolFlix</h1>
+        <SearchBar onSearch={searchMedia} />
+      </header>
+      <MovieList movies={results} imageBaseUrl={IMAGE_BASE_URL} />
     </div>
   );
 }
 
 export default App;
+
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,45 +1,12 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar } from '@fortawesome/free-solid-svg-icons';  // Usato sia per stella piena che vuota
+import MovieCard from "./MovieCard";
 
-function MovieList({ movies, languageCodes, imageBaseUrl }) {
+function MovieList({ movies, imageBaseUrl }) {
   return (
-    <ul>
-      {movies.map((media) => {
-        const title = media.title || media.name;
-        const originalTitle = media.original_title || media.original_name;
-
-        // URL per la bandiera
-        const flagUrl = `https://flagcdn.com/w40/${languageCodes[media.original_language] || 'un'}.png`;
-
-        // URL dell'immagine del poster 
-        const imageUrl = media.poster_path
-          ? `${imageBaseUrl}w300${media.poster_path}` 
-          : "https://via.placeholder.com/342x500"; 
-
-        // arrotondamento da 1 a 10 ad 1 a 5
-        const rating = Math.ceil(media.vote_average / 2);
-
-        return (
-          <li key={media.id}>
-            <img src={imageUrl} alt={title} />
-            <h3>{title} ({originalTitle})</h3>
-            <p>Lingua: <img src={flagUrl} alt={media.original_language} /></p>
-            <p>Voto: 
-              <span>
-                {Array.from({ length: 5 }, (_, i) => 
-                  i < rating  
-                
-                  // Stella piena
-                  ? <FontAwesomeIcon key={i} icon={faStar} color="gold" />
-                  // Stella vuota 
-                  : <FontAwesomeIcon key={i} icon={faStar} color="gray" />  
-                )}
-              </span>
-            </p>
-          </li>
-        );
-      })}
-    </ul>
+    <div className="movie-list">
+      {movies.map((movie) => (
+        <MovieCard key={movie.id} movie={movie} imageBaseUrl={imageBaseUrl} />
+      ))}
+    </div>
   );
 }
 
