54f9f30f469b504d455b6dc569320f5715a4104c
Federico Spadaro <federicospadaro2@gmail.com>
2025-02-28 18:13:12 +0100

    [ADD] new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
+// App.jsx
 import { useState } from "react";
 import axios from "axios";
 import SearchBar from "./components/SearchBar";
 import MovieList from "./components/MovieList";
 
 const API_KEY = "3d2f7b4506c7c6af335517dd5f8c25b0";
-const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
-
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w342";
 const LANGUAGE_CODES = {
   en: "gb",
   it: "it",
@@ -13,7 +13,7 @@ const LANGUAGE_CODES = {
   es: "es",
   de: "de",
   ja: "jp",
-  ko: "kr",
+  ko: "kr"
 };
 
 function App() {
@@ -21,30 +21,30 @@ function App() {
 
   const searchMedia = async (query) => {
     if (!query) return;
+
     try {
       const movieResponse = await axios.get(
         `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}&language=it-IT`
       );
+
       const tvResponse = await axios.get(
         `https://api.themoviedb.org/3/search/tv?api_key=${API_KEY}&query=${query}&language=it-IT`
       );
-      const combinedResults = [...movieResponse.data.results, ...tvResponse.data.results];
-      setResults(combinedResults);
+
+      // Unire i risultati di film e serie
+      setResults([...movieResponse.data.results, ...tvResponse.data.results]);
     } catch (error) {
       console.error("Errore nella chiamata API", error);
     }
   };
 
   return (
-    <div className="app">
-      <header className="app-header">
-        <h1>BoolFlix</h1>
-        <SearchBar onSearch={searchMedia} />
-      </header>
-      <MovieList movies={results} imageBaseUrl={IMAGE_BASE_URL} />
+    <div>
+      <h1>BoolFlix</h1>
+      <SearchBar onSearch={searchMedia} />
+      <MovieList movies={results} languageCodes={LANGUAGE_CODES} imageBaseUrl={IMAGE_BASE_URL} />
     </div>
   );
 }
 
 export default App;
-
